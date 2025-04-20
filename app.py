114e8b6bc10b380d7ab4fddbeb07e28f70000cf9
Ankit3773 <150064536+Ankit3773@users.noreply.github.com>
2025-04-20 15:00:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,23 +12,23 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
 
 # Page config
-st.set_page_config(page_title="Salary Negotiation Chatbot", page_icon="💼")
+st.set_page_config(page_title="AI Custom Meal Plan Generator", page_icon="🥗")
 
 # 👇 Custom Header
 st.markdown(
     """
     <div style='text-align: center; padding: 10px; border-bottom: 2px solid #ddd;'>
-        <h3>ANKIT KUMAR - 12319407</h3>
-        <h3>AAYUSH KUMAR - 12319413</h3>
+        <h3>ANKIT RAj - 12319449</h3>
+        <h3>AMITABH KAUSHIK- 12319369</h3>
     </div>
     """,
     unsafe_allow_html=True
 )
 
 # App title and intro
-st.title("💼 Salary Negotiation Chatbot")
-st.markdown("Ask me anything about negotiating your salary!")
-st.success("👋 Welcome! Ask me anything about salary negotiations. Let's boost your paycheck!")
+st.title("🥗 AI Custom Meal Plan Generator")
+st.markdown("Ask me anything about Meal Plan!")
+st.success("👋 !")
 
 # Store chat history in session
 if "messages" not in st.session_state:
